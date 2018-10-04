341005e655663215503451fdd2baf3752b63f8eb
Julien Goux <julien.goux@live.fr>
2018-10-04 13:48:43 +0200

    Change component propType
    As we pass a class constructor instead of an instance of a class, the right type is `func` (see for example react-router's component propType :     component: https://github.com/ReactTraining/react-router/blob/master/packages/react-router/modules/Route.js#L130 )

--- src/TunnelPlaceholder.js
+++ src/TunnelPlaceholder.js
@@ -4,7 +4,7 @@ import React, { Component, Fragment } from 'react'
 class TunnelPlaceholder extends Component {
   static propTypes = {
     children: PropTypes.func,
-    component: PropTypes.oneOfType([PropTypes.node, PropTypes.symbol]),
+    component: PropTypes.func,
     id: PropTypes.string.isRequired,
     multiple: PropTypes.bool,
   }
